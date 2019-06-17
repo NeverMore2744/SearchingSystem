83ec69f2fcbab16723fd85a51b09ee60634393cf
NeverMore2744 <ljh199707@163.com>
2019-06-18 02:36:14 +0800

    add boundary condition checking for wn.synset

--- SearchSystem/Serching/searchWord.py
+++ SearchSystem/Serching/searchWord.py
@@ -7,7 +7,7 @@ from Serching import operateDocList
 
 def search_single_word(index, word, syn_FLAG):
     if syn_FLAG:
-        synonym = wn.synsets(word)[0].lemma_names()
+        synonym = wn.synsets(word)[0].lemma_names() if len(word) > 0 else []
     else:
         synonym = [word]
     res = []
